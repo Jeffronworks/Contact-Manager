ab589fdad083865082f3b5d66cd524397ced63e7
Jeffronworks <37477585+Jeffronworks@users.noreply.github.com>
2018-12-07 04:57:25 +0100

    did some clean and restored browser router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AddContact from "./components/contacts/AddContact";
 import EditContact from "./components/contacts/EditContact";
 import About from "./components/pages/About";
